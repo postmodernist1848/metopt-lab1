a2b9b49e997f4d8b46714fbf04dba3c4b0c25c40
Vladimir Ivanov <elitic.pantheism@gmail.com>
2025-04-24 03:05:33 +0300

    refactor creating data for a table

---
+++ mktable.py
@@ -0,0 +1,43 @@
+from algorithms import *
+from funcs import q1, q2, f4, fsinsin
+from stats import BiFuncStatsDecorator, print_stats
+
+def main():
+    xs = [np.array(x) for x in ([-1, -1], [1.0, 4.0], [1.0, 1.0])]
+
+    funcs = [
+        ("q1", BiFuncStatsDecorator(q1)), # "x.T*[[1, 0], [0, 1]]*x + [1, 1]*x - 1.4"
+        ("q2", BiFuncStatsDecorator(q2)), # "x.T*[[0.1, 0], [0, 3]]*x"
+        ("f4", BiFuncStatsDecorator(f4)), # "(x**2 - 1)**2 + y**2 + 0.5 * x"
+        ("sin(x) + sin(y)", BiFuncStatsDecorator(fsinsin))
+    ]
+
+    stop_condition = relative_x_condition()
+
+    algorithms = [
+        ("Learning rate scheduling const(0.1)", lambda args: learning_rate_scheduling(args['x_0'], args['func'], constant_h(0.1), stop_condition)),
+        ("Learning rate scheduling exp(0.5)", lambda args: learning_rate_scheduling(args['x_0'], args['func'], exponential_decay(0.5), stop_condition)),
+        ("Learning rate scheduling exp(0.3)", lambda args: learning_rate_scheduling(args['x_0'], args['func'], exponential_decay(0.3), stop_condition)),
+        ("Armijo Gradient Descent", lambda args: steepest_gradient_descent_armijo(args['x_0'], args['func'], stop_condition)),
+        ("Dichotomy Gradient Descent", lambda args: steepest_gradient_descent_dichotomy(args['x_0'], args['func'], args['dichotomy_eps'], stop_condition)),
+        ("Scipy Wolfe Gradient Descent", lambda args: steepest_gradient_descent_scipy_wolfe(args['x_0'], args['func'], stop_condition)),
+        ("Newton Descent with 1D Search", lambda args: newton_descent_with_1d_search(args['x_0'], args['func'], stop_condition, armijo_step_selector)),
+        ("Dog Leg const(0.1)", lambda args: damped_newton_descent(args['x_0'], args['func'], stop_condition, constant_h(0.1)))
+    ]
+
+    eps = 1e-9
+    for algorithm_name, applier in algorithms:
+        for func_name, func in funcs:
+            for x_0 in xs:
+                args = {
+                    "x_0": x_0,
+                    "func": func,
+                    "stop_condition": relative_x_condition(),
+                    "dichotomy_eps": eps
+                }
+
+                trajectory = applier(args)
+                print_stats(func, trajectory,  f'{func_name} | {algorithm_name} | x0={x_0}', plot=False)
+        
+if __name__ == "__main__":
+    main()
---
+++ stats.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from algorithms import BiFunc
+
+class BiFuncStatsDecorator:
+    f: BiFunc
+    call_count: int = 0
+    gradient_count: int = 0
+    hessian_count: int = 0
+
+    def __init__(self, f: BiFunc):
+        self.f = f
+
+    def __call__(self, x: np.ndarray):
+        self.call_count += 1
+        return self.f.__call__(x)
+
+    def gradient(self, x: np.ndarray) -> np.ndarray:
+        self.gradient_count += 1
+        return self.f.gradient(x)
+
+    def hessian(self, x: np.ndarray) -> np.ndarray:
+        self.hessian_count += 1
+        return self.f.hessian(x)
+
+    def reset(self):
+        self.hessian_count = self.gradient_count = self.call_count = 0
+
+    def min(self) -> float | None:
+        return self.f.min()
+
+PLOT_SIZE=3
+
+def plot_trajectory(func: BiFunc, trajectory: np.ndarray, title=None, plot_size=PLOT_SIZE):
+    # Create a meshgrid for the 3D plot
+    x = np.linspace(-PLOT_SIZE, PLOT_SIZE, 100)
+    y = np.linspace(-PLOT_SIZE, PLOT_SIZE, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = np.zeros_like(X)
+
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            Z[i, j] = func(np.array([X[i, j], Y[i, j]]))
+
+    # Plot the 3D surface
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.6)  # type: ignore
+
+    # Plot the trajectory
+    ax.plot(trajectory[:, 0], trajectory[:, 1], [func(np.array([x, y]))
+            for x, y in trajectory], color='r', marker='o')
+
+    ax.scatter(trajectory[-1, 0], trajectory[-1, 1], func(trajectory[-1]), color='b', label='Final point')
+
+    if title:
+        ax.set_title(title)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')  # type: ignore
+    plt.show()
+
+def print_stats(func: BiFuncStatsDecorator, trajectory: np.ndarray, title=None, plot=True):
+    cc, gc, hc = func.call_count, func.gradient_count, func.hessian_count
+    calculated_min = func(trajectory[-1])
+    print(f'title: {title}')
+    print(f'Iterations: {len(trajectory) - 1}')
+    print(f'x: {trajectory[-1]} f(x): {calculated_min}')
+    print(f'Function evaluations: {cc}')
+    print(f'Gradient evaluations: {gc}')
+    print(f'Hessian evaluations: {hc}')
+    min_value = func.min()
+    if min_value is not None:
+        print(f'True minimum: {min_value}')
+        print(f'Error: {abs(calculated_min - min_value)}')
+    print()
+    if plot:
+        plot_trajectory(func, trajectory, title)
+    func.reset()
