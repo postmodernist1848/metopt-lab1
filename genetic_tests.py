cc811fcafd2a01b6111415f5e3b00f21e9720936
Denis <fafa.dadad@mail.ru>
2025-06-07 01:22:01 +0300

    add crossover_commivoyager smart version

--- genetic_tests.py
+++ genetic_tests.py
@@ -19,6 +19,89 @@ def crossover_commivoyager(parent1: Vector, parent2: Vector) -> Vector:
     child[prefix_index:] = remaining
     return child
 
+def build_paths(path: dict) -> list:
+    paths = []
+    used_points = set()
+    
+    for start_point in path:
+        if start_point in used_points:
+            continue
+            
+        current_path = [start_point]
+        used_points.add(start_point)
+        current_point = start_point
+        
+        while True:
+            next_points = path[current_point]
+            if not next_points:
+                break
+                
+            next_point = None
+            for point in next_points:
+                if point not in used_points:
+                    next_point = point
+                    break
+                    
+            if next_point is None:
+                break
+                
+            current_path.append(next_point)
+            used_points.add(next_point)
+            current_point = next_point
+            
+        if len(current_path) > 1:
+            paths.append(current_path)
+    return paths
+
+def build_path_dict(distance: list) -> dict:
+    path = {}
+    for _, points in distance:
+        key1 = points[0]
+        key2 = points[1]
+        if key1 not in path:
+            path[key1] = []
+        path[key1].append(key2)
+        if key2 not in path:
+            path[key2] = []
+        path[key2].append(key1)
+    return path
+
+def get_distance(parent: Vector) -> list:
+    distance = []
+    n = len(parent)
+    prev_point = parent[0]
+    for i in range(1, n + 1):
+        point = parent[i % n]
+        distance.append((np.linalg.norm(prev_point - point), [tuple(prev_point), tuple(point)]))
+        prev_point = point
+
+    distance.sort(key=lambda x: x[0])
+    return distance
+
+def crossover_commivoyager_smart(parent1: Vector, parent2: Vector) -> Vector:
+    n = len(parent1)
+    alpha = np.random.randint(0, n)
+
+    distance = get_distance(parent1)
+    distance = distance[:alpha]
+
+    path = build_path_dict(distance)
+    paths = build_paths(path)
+
+    merged_path = []
+    for path in paths:
+        merged_path.extend(path)
+
+    merged_path_set = set(merged_path)
+    diff = []
+    
+    for point in parent2:
+        if tuple(point) not in merged_path_set:
+            diff.append(point)
+            
+    merged_path.extend(diff)
+    return merged_path
+
 def mutate_commivoyager(individual: Vector, mutation_rate: float = 0.1) -> Vector:
     if np.random.random() < mutation_rate:
         size = len(individual)
@@ -58,17 +141,17 @@ def commivoyager_genetic_test(points: Vector, correct_value: float = None):
     population = init_population_commivoyager(x0, 100)
     func = BiFuncCallableWrapper(commivoyager, correct_value)
     test_genetic_algorithm(population, 
-                         crossover_commivoyager,
+                         crossover_commivoyager_smart,
                          mutate_commivoyager,
-                         func, 30, 0.1, 1e-4)
+                         func, 60, 0.1, 1e-2)
     
     x = min(population, key=func)
     commivoyager_plot(x0, x, "Initial vs Optimized path")
 
 if __name__ == "__main__":
-    f4_genetic_test()
-    # commivoyager_genetic_test(
-    #     np.array([[0, 0], [2, 0], [4, 0],
-    #                [0, 2], [0, 4], [-2, 0],
-    #                  [-4, 0], [0, -2], [0, -4],
-    #                    [1, 1]]), 26.14213562373095)
+    # f4_genetic_test()
+    commivoyager_genetic_test(
+        np.array([[0, 0], [2, 0], [4, 0],
+                   [0, 2], [0, 4], [-2, 0],
+                     [-4, 0], [0, -2], [0, -4],
+                       [1, 1]]), 26.14213562373095)
